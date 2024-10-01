def2493a5e658155aee620105d99992e571525f8
ongaro09 <ongaromark7@gmail.com>
2024-10-02 01:54:02 +0300

    modified user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,27 +1,8 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['user', 'admin'], default: 'user' },
-  playlists: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Playlist' }]
-}, { timestamps: true });
-
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
-  next();
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
 });
-
-userSchema.methods.comparePassword = async function(candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
-};
-
-userSchema.methods.generateAuthToken = function() {
-  return jwt.sign({ userId: this._id, role: this.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
-};
 
 module.exports = mongoose.model('User', userSchema);
