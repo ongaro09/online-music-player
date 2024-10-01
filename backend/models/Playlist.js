b748d68df4cf868310b2319ebe82c7f7aa00ac1a
ongaro09 <ongaromark7@gmail.com>
2024-10-02 02:03:58 +0300

    updated playlists model

--- backend/models/Playlist.js
+++ backend/models/Playlist.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 const playlistSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  songs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Song' }]
-}, { timestamps: true });
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    name: { type: String, required: true },
+    songs: [{ type: String }], // Store SoundCloud song IDs or URLs
+}, {
+    timestamps: true,
+});
 
 module.exports = mongoose.model('Playlist', playlistSchema);
