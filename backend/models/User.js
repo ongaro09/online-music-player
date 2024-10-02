4211880207595b3d63424047364035eaae199a05
ongaro09 <ongaromark7@gmail.com>
2024-10-02 06:09:48 +0300

    created a new model for user

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    playlists: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Playlist' }],
+}, {
+    timestamps: true,
 });
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
